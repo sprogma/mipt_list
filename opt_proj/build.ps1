a57e2f5c12f3032ea16f461160b24a18ee893a07
sprogma <mux-228@yandex.ru>
2025-11-04 06:34:50 +0300

    regenerated image

--- opt_proj/build.ps1
+++ opt_proj/build.ps1
@@ -1,1 +1,1 @@
-gcc -x c++ test.cpp -x c super_list.c -o a.exe -Ofast -mprfchw -march=native -DNDEBUG -flto -lstdc++ -g
+cp ..\super_list.c; gcc -x c++ test.cpp -x c super_list.c -o a.exe -Ofast -mprfchw -march=native -DNDEBUG -flto -lstdc++ -g
